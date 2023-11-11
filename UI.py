05ba2c67c458a39ec74512ea49882cecb3be13fb
Tri <dinhtrikt11102002@gmail.com>
2023-11-11 23:23:55 +0700

    update

--- UI.py
+++ UI.py
@@ -19,8 +19,7 @@ def select_directory():
 def preprocess_data():
     try:
         processed_data = Preprocess(data)
-        # Thực hiện các công việc cần thiết với dữ liệu đã được xử lý
-        # Ví dụ: In kích thước của dữ liệu sau khi xử lý
+            
         print(processed_data.shape)
     except Exception as e:
         status_label.config(text=f"Lỗi khi xử lý dữ liệu: {str(e)}")
@@ -33,7 +32,9 @@ title_label = tk.Label(root, text="Ứng dụng sử dụng PCA để giảm chi
 note_label = tk.Label(root, text="Ứng dụng lấy đặc trưng của ảnh bằng cách flatten các pixel có trong ảnh và sử dụng các đặc trưng đã lấy để phân lớp ảnh bằng thuật toán K-means")
 user_manual = tk.Label(root, text="Hướng dẫn sử dụng")
 user_manual_1 = tk.Label(root, text="1. Chọn đường dẫn chứa tập ảnh có cùng kích thước đầu vào.")
-user_manual_2 = tk.Label(root, text="1. Chọn phương pháp lấy đặc trưng ảnh.")
+user_manual_2 = tk.Label(root, text="2. Chọn phương pháp lấy đặc trưng ảnh.")
+user_manual_3 = tk.Label(root, text="2. Chọn bắt đầu dữ liệu sẽ được phân lớp bằng K-means. Kết quả sẽ được so sánh trên cả 2 tập dữ liệu trước và sau PCA")
+
 
 blank = tk.Label(root, text="")
 status_label = tk.Label(root, text="")
@@ -42,6 +43,9 @@ blank.pack()
 note_label.pack()
 
 user_manual.pack()
+user_manual_1.pack()
+user_manual_2.pack()
+user_manual_3.pack()
 
 root.title("Chọn Đường Dẫn")
 
@@ -51,7 +55,10 @@ select_button.pack(pady=20)
 
 # Tạo button để chọn cách lấy vector đặc trưng
 label_1 = tk.Label(root, text="Lấy đặc trưng ảnh")
-Histogram_button = tk.Button(root, text="Duỗi thẳng pixel", command=preprocess_data)
+label_1.pack()
+Flatten_button = tk.Button(root, text="Flatten pixel", command=preprocess_data)
+Histogram_button = tk.Button(root, text="Flatten pixel", command=Histogram)
+
 Histogram_button.pack(pady=20)
 
 # Thông báo
