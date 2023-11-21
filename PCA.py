4b28633c085a4ce06cb51bd05e3605fed70f1195
Tri <dinhtrikt11102002@gmail.com>
2023-11-21 18:49:46 +0700

    update

--- PCA.py
+++ PCA.py
@@ -5,10 +5,7 @@ from sklearn.cluster import KMeans
 import time
 from skimage.transform import resize
 from skimage.feature import hog
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
+
 
 #Đọc ảnh từ thư mục
 def ReadImage(Path, size):
@@ -89,57 +86,3 @@ def PCA(feature, keep=0.9):
     transformed = np.dot(standardized_data.T, components)
 
     return transformed, ratio
-
-# Mô hình nhỏ để phân lớp
-# class Classify(nn.Module):
-#     def __init__(self, in_features, out_features):
-#         super(Classify, self).__init__()
-#         self.Layer = torch.nn.Sequential(
-#              nn.Linear(in_features, 1024),
-#              nn.ReLU(),
-#              nn.Linear(1024, 512),
-#              nn.ReLU(),
-#              nn.Linear(512, 256),
-#              nn.ReLU(),
-#              nn.Linear(256, out_features)
-#         )
-
-#     def forward(self, x):
-#         return self.Layer(x)
-
-    
-            
-
-
-# def main():
-#     #feature_extraction = ReadImage(PATH) # Path là đường dẫn đến folder chứa ảnh
-#     # Đang mặc định để thử hàm
-#     feature_extraction = np.random.randint(0,255,(500,256,256))
-#     feature = Preprocess(feature_extraction)
-#     print("---Lay thanh phan chinh bang pca---")
-#     #Chạy PCA
-#     start = time.time()
-#     data_PCA = PCA(feature)
-#     end = time.time()
-#     print ("Thoi gian chay PCA: {}".format(end-start))
-#     print()
-    
-#     # Kmean với dữ liệu không sử dụng PCA
-#     start = time.time()
-#     kmeans = KMeans(n_clusters=3, random_state=0, n_init="auto").fit(feature)
-#     end = time.time()
-#     print ("So chieu dau vao khong su dung PCA: {}".format(feature.shape[1]))
-#     print ("Thoi gian chay khi khong su dung PCA: {}".format(end-start))
-#     print()
-    
-#     # Kmean với dữ liệu sử dụng PCA
-#     start = time.time()
-#     print("Lượng data được giữ lại: {}%".format(data_PCA[1]))
-#     print ("So chieu dau vao su dung PCA: {}".format(data_PCA[0].shape[1]))
-#     kmeans = KMeans(n_clusters=3, random_state=0, n_init="auto").fit(data_PCA[0])
-    
-#     end = time.time()
-#     print ("Thoi gian chay khi su dung PCA: {}".format(end-start))
-
-# if __name__=="__main__":
-#     main()
